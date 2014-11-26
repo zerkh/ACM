9cc80acaa3a2a14a9574ae8c765a0b97a12392bb
zerkh <mhzx20@163.com>
2014-11-26 23:04:09 +0800

    Nov 26 23:01

--- 1009.cpp
+++ 1009.cpp
@@ -3,6 +3,28 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#define MAXSIZE 2000  
 using namespace std;
 
-int Compare
+typedef struct  PixInfo
+{
+  int pos;
+  int code;
+}Pix;
+
+Pix inPairMap[MAXSIZE];
+Pix outMap[8*MAXSIZE];
+
+void Sort(int size);
+int GetVal(int pos);
+int GetCode(int pos);
+void CalOutMap(int size);
+
+int main()
+{
+  int size = 0;
+  
+  
+  return 0;
+}
+
---
+++ 1009.cpp~
@@ -0,0 +1,8 @@
+//通过对每一个边界数值进行处理完成对pair的遍历
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int Compare
